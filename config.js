6bcc4468e51960a096fb5d615ccb5c2f0f27d1e4
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-07 06:57:06 -0700

    Change API_ENDPOINT value to point to the production URL

--- config.js
+++ config.js
@@ -1,8 +1,26 @@
 // config.js
 export const config = {
-    API_ENDPOINT: 'http://localhost:8080/',
+    API_ENDPOINT: 'https://dolphin-app-dchbn.ondigitalocean.app/',
     UPPERCASE_COMPANIES: new Set([
-        'AMEX', 'DHL', 'IBM', 'UPS', 'TD', 'CHASE', 'USPS', 'AT&T',
-        'NASA', 'CNN', 'HP', 'JFK', 'H&M', 'IKEA', 'BMW', 'KFC', 'EA', '3M', 'GE', 'LG'
+      'AMEX',
+      'DHL',
+      'IBM',
+      'UPS',
+      'TD',
+      'CHASE',
+      'USPS',
+      'AT&T',
+      'NASA',
+      'CNN',
+      'HP',
+      'JFK',
+      'H&M',
+      'IKEA',
+      'BMW',
+      'KFC',
+      'EA',
+      '3M',
+      'GE',
+      'LG'
     ]),
-};
+  };
