78feb8cb7d2b0a0a42a27c924602cdc5f7ce189e
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-08 10:09:34 -0700

    Implement and refine login modal feature with enhanced UX

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Navigation from './Navigation';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+    <UserProvider>
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </UserProvider>
+    </html>
+  );
+}
