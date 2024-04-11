e2cda489eb69c9658aca1dad7605eef982e4a737
Pejman H Nia <81389644+pejmantheory@users.noreply.github.com>
2024-04-10 22:15:00 -0700

    Removed login Netlify Identify Codes, since we don't need it for now.

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
 import { config } from './config.js';
 import { capitalizeCompany, displayNotification, isValidURL, delay } from './utils.js';
 
-const auth = new GoTrue({
-  APIUrl: 'https://helloblue.ai/.netlify/identity',
-  setCookie: true,
-  cookieOptions: {
-    SameSite: 'None',
-    Secure: true,
-  },
-});
-
-const login = async (email, password) => {
-  try {
-    const response = await auth.login(email, password);
-    console.log("Success! Response: ", response);
-    window.location.href = '/dashboard';
-  } catch (error) {
-    console.error("Failed to login: ", error);
-    displayNotification("Login failed. Please check your credentials and try again.");
-  }
-};
 
 document.addEventListener('DOMContentLoaded', () => {
   const elements = {
