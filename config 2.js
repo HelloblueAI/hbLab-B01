b9c5ad42c14e13afcef705c48ea2db22d74c27fe
Pejman Haghighatnia <pejmanhnia@gmail.com>
2024-05-23 22:00:07 -0700

    Add untracked files

---
+++ config 2.js
@@ -0,0 +1,25 @@
+export const config = {
+  API_ENDPOINT: 'https://dolphin-app-dchbn.ondigitalocean.app/api/company',
+  UPPERCASE_COMPANIES: new Set([
+    'AMEX',
+    'DHL',
+    'IBM',
+    'UPS',
+    'TD',
+    'CHASE',
+    'USPS',
+    'AT&T',
+    'NASA',
+    'CNN',
+    'HP',
+    'JFK',
+    'H&M',
+    'IKEA',
+    'BMW',
+    'KFC',
+    'EA',
+    '3M',
+    'GE',
+    'LG'
+  ]),
+};
