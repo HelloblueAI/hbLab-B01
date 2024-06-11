a7faa66c1b04ddc1a38fe2a6b6372fc35b4fa1da
Pejman Haghighatnia <pejmanhaghighatnia@Pejmans-MacBook-Pro.local>
2024-06-10 21:31:24 -0700

    Remove obsolete files

--- app/logoutpage.jsx
+++
@@ -1,9 +0,0 @@
-export default function LogoutPage() {
-    return (
-      <div>
-        <h1>Logout</h1>
-        <p>You have been logged out.</p>
-        <a href="/">Go to Home</a>
-      </div>
-    );
-  }
